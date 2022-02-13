045cc2465217833e3c8d11101ef9332a5440045f
Руслан <Fishander@mail.ru>
2022-02-13 14:15:55 +0300

    а

--- XRoy/ViewController.swift
+++ XRoy/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var array = Array<String>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-    
-    
 }
 
