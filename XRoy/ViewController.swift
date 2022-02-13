da58d413d8edec5f8612a6fc0a37a800be1a2d39
Руслан <Fishander@mail.ru>
2022-02-13 12:10:06 +0300

    Initial Commit

---
+++ XRoy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XRoy
+//
+//  Created by Admin on 13.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
